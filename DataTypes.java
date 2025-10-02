af2f563502d285546874025188422d1c3d8cd10c
Sasi-Galaxy <sasikumardharmaraj08@gmail.com>
2025-10-02 19:06:05 +0530

    ADD

---
+++ DataTypes.java
@@ -0,0 +1,24 @@
+package JAVA_CORE;
+
+public class DataTypes {
+    public static void main(String[] args) {
+       int n=10;
+       float f=9.9f;
+       double d=99.99;
+       char c='D';
+       String s="SASI";
+       boolean a=true;
+       
+       System.out.println("Integer:"+n);
+       System.out.println("Float:"+f);
+       System.out.println("Double:"+d);
+       System.out.println("Character:"+c);
+       System.out.println("String:"+s);
+       System.out.println("Boolean:"+a);
+
+
+
+          
+
+    }
+}
